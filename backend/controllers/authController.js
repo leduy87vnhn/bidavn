d2cd50d13a847b3ffe8b0abdaf343fc35eb94bbb
leduy87vnhn <leduy87vnhn@gmail.com>
2025-04-20 01:35:59 +0700

    fix bug

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,6 +37,17 @@ const registerUser = async (req, res) => {
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
             RETURNING id, user_name, user_type, created_date
         `;
+        console.log('Parameters being passed:');
+        console.log('1 user_name      =', user_name);
+        console.log('2 name           =', name);
+        console.log('3 hashedPassword =', hashedPassword);
+        console.log('4 user_type      =', user_type);
+        console.log('5 birthday       =', birthday);
+        console.log('6 phone_number   =', phone_number);
+        console.log('7 email          =', email);
+        console.log('8 created_date   =', created_date);
+        console.log('9 modified_date  =', modified_date);
+        console.log('10 enable        =', false); // dùng boolean thật
         const result = await client.query(query, [user_name, name, hashedPassword, user_type, birthday, phone_number, email, created_date, modified_date, false]);
         console.log('Insert successful');
 
