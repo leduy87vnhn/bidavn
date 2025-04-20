38b58624512dbd0adc439030260c05fe3d5e3946
leduy87vnhn <leduy87vnhn@gmail.com>
2025-04-20 22:22:26 +0700

    update server IP

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use(cors({
-    origin: '*',              // Cho phép mọi nguồn gọi vào
-    methods: ['GET', 'POST'], // Cho phép các method
+    origin: '*', // Nếu muốn mở toàn bộ
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
 
