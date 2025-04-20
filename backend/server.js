06f58aedd129369648993e3f70121494295a1748
leduy87vnhn <leduy87vnhn@gmail.com>
2025-04-20 23:41:27 +0700

    fix bug cors

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,18 @@ const app = express();
 const port = 5000;
 
 app.use('/api/auth', authRoutes);
-app.use(cors({
-    origin: '*', // Nếu muốn mở toàn bộ
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+const whitelist = ['http://localhost:3000', 'http://18.141.197.31:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 console.log("Server starting...");
