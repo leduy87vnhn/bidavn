001f1a5f43e6c14f078c1300ac03c33fbfbcadf0
leduy87vnhn <leduy87vnhn@gmail.com>
2025-05-03 10:15:28 +0700

    fix bug directory

--- frontend/src/components/TournamentRegistration.js
+++ frontend/src/components/TournamentRegistration.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
-import './tournamentRegistration.scss';
+import '../tournamentRegistration.scss';
 
 const TournamentRegistration = () => {
   const { id: tournamentId } = useParams();
