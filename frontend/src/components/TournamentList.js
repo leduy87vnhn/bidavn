eef0592e8e8de92372afab9759f1a4dd8de8759e
leduy87vnhn <leduy87vnhn@gmail.com>
2025-07-13 00:52:03 +0700

    improve UI

--- frontend/src/components/TournamentList.js
+++ frontend/src/components/TournamentList.js
@@ -576,22 +576,22 @@ const TournamentList = () => {
                                         </td>
                                         {/* <td style={{ border: '1px solid #ddd', padding: '8px' }}>{formatDate(tour.start_date)}</td>
                                         <td style={{ border: '1px solid #ddd', padding: '8px' }}>{formatDate(tour.end_date)}</td> */}
-                                        {/* <td style={{
-                                        border: '1px solid #ddd',
-                                        padding: '8px',
-                                        maxHeight: '100px',
-                                        overflowY: 'auto',
-                                        whiteSpace: 'pre-wrap',
-                                        wordBreak: 'break-word'
+                                        <td style={{
+                                            border: '1px solid #ddd',
+                                            padding: '8px',
+                                            maxHeight: '100px',
+                                            overflowY: 'auto',
+                                            whiteSpace: 'pre-wrap',
+                                            wordBreak: 'break-word'
                                         }}>{tour.location}</td>
                                         <td style={{
-                                        border: '1px solid #ddd',
-                                        padding: '8px',
-                                        maxHeight: '100px',
-                                        overflowY: 'auto',
-                                        whiteSpace: 'pre-wrap',
-                                        wordBreak: 'break-word'
-                                        }}>{tour.prize}</td> */}
+                                            border: '1px solid #ddd',
+                                            padding: '8px',
+                                            maxHeight: '100px',
+                                            overflowY: 'auto',
+                                            whiteSpace: 'pre-wrap',
+                                            wordBreak: 'break-word'
+                                        }}>{tour.prize}</td>
 
                                         <td style={{
                                             border: '1px solid #ddd',
