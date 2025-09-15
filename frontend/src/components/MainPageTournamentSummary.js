b02473282275e62000bb838c0d1bff887a62b160
leduy87vnhn <leduy87vnhn@gmail.com>
2025-09-16 01:52:11 +0700

    enable THao Tác column

--- frontend/src/components/MainPageTournamentSummary.js
+++ frontend/src/components/MainPageTournamentSummary.js
@@ -62,7 +62,7 @@ const MainPageTournamentSummary = () => {
       <div className="summary-header">GIẢI THỂ THAO</div>
       {groups
       .slice() // copy ra để không làm mutate state
-      .sort((a, b) => new Date(a.start_date) - new Date(b.start_date)) // sắp xếp tăng dần theo start_date
+      .sort((a, b) => new Date(b.start_date) - new Date(a.start_date)) // sắp xếp tăng dần theo start_date
       .map((item) => (
         <div key={item.id} className="summary-row">
           <div
