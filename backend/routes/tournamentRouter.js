895f5c96006b3db6f7273bb1a8c6a049a14e6320
leduy87vnhn <leduy87vnhn@gmail.com>
2025-09-22 19:45:47 +0700

    improve counting competitors quantity

--- backend/routes/tournamentRouter.js
+++ backend/routes/tournamentRouter.js
@@ -374,6 +374,7 @@ router.get('/groups-with-events', async (req, res) => {
         e.fee_label_rank2,
         e.fee_label_rank3,
         e.maximum_competitors,
+        e.background_image,
         e.registration_deadline,
         (
           SELECT COUNT(*)
@@ -421,6 +422,7 @@ router.get('/groups-with-events', async (req, res) => {
           fee_label_rank2: row.fee_label_rank2,
           fee_label_rank3: row.fee_label_rank3,
           maximum_competitors: row.maximum_competitors,
+          background_image: row.background_image,
           registration_deadline: row.registration_deadline,
           approved_competitors_count: row.approved_competitors_count
         });
